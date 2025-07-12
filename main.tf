8c3dec94568180483d63df0f3385e8bb13a35e24
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-12 11:23:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,9 @@ provider "aws" {
 
 # VPC and Subnet
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "public" {
@@ -34,12 +36,14 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
+
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -48,15 +52,17 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
+# ECS Cluster
 resource "aws_ecs_cluster" "cluster" {
   name = "medusa-cluster"
 }
 
-# Use existing IAM Role for ECS Task Execution
+# Use existing IAM role
 data "aws_iam_role" "ecs_task_execution" {
   name = "ecsTaskExecutionRole"
 }
 
+# Task Definition
 resource "aws_ecs_task_definition" "medusa_task" {
   family                   = "medusa-task"
   requires_compatibilities = ["FARGATE"]
@@ -65,28 +71,39 @@ resource "aws_ecs_task_definition" "medusa_task" {
   memory                   = "1024"
   execution_role_arn       = data.aws_iam_role.ecs_task_execution.arn
 
-  container_definitions = jsonencode([{
-    name      = "medusa",
-    image     = "medusajs/medusa:v1.13.1", # Use a valid Medusa version tag
-    portMappings = [{ containerPort = 9000 }],
-    environment = [
-      {
-        name  = "DATABASE_URL",
-        value = "sqlite://./medusa-db.sqlite"
-      }
-    ]
-  }])
+  container_definitions = jsonencode([
+    {
+      name      = "medusa"
+      image     = "ghcr.io/medusajs/medusa:v1.13.1"
+      portMappings = [{
+        containerPort = 9000
+      }]
+      environment = [
+        {
+          name  = "DATABASE_URL"
+          value = "sqlite://./medusa-db.sqlite"
+        }
+      ]
+    }
+  ])
 }
 
+# ECS Service
 resource "aws_ecs_service" "service" {
   name            = "medusa-service"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.medusa_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+
   network_configuration {
     subnets          = [aws_subnet.public.id]
     security_groups  = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
   }
+}
+
+# Output
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.cluster.name
 }
