3d5943a9a07aa2a0f9326476b724817349ab591a
LITHESH MALEPATI <malepatilithu213@gmail.com>
2025-07-12 11:19:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,29 +2,44 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Use default VPC
-data "aws_vpc" "default" {
-  default = true
+# VPC and Subnet
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-# Use the default subnet IDs from the default VPC
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 }
 
-# Create a Security Group
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_security_group" "ecs_sg" {
-  name        = "ecs_sg"
-  description = "Allow all inbound and outbound"
-  vpc_id      = data.aws_vpc.default.id
-
+  vpc_id = aws_vpc.main.id
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -33,17 +48,15 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-# Create ECS Cluster
 resource "aws_ecs_cluster" "cluster" {
   name = "medusa-cluster"
 }
 
-# Use the existing execution role created by AWS (no IAM conflict)
+# Use existing IAM Role for ECS Task Execution
 data "aws_iam_role" "ecs_task_execution" {
   name = "ecsTaskExecutionRole"
 }
 
-# ECS Task Definition for Medusa
 resource "aws_ecs_task_definition" "medusa_task" {
   family                   = "medusa-task"
   requires_compatibilities = ["FARGATE"]
@@ -52,41 +65,28 @@ resource "aws_ecs_task_definition" "medusa_task" {
   memory                   = "1024"
   execution_role_arn       = data.aws_iam_role.ecs_task_execution.arn
 
-  container_definitions = jsonencode([
-    {
-      name  = "medusa",
-      image = "medusajs/medusa:v1.13.1",
-      portMappings = [
-        {
-          containerPort = 9000
-        }
-      ],
-      environment = [
-        {
-          name  = "DATABASE_URL",
-          value = "sqlite://./medusa-db.sqlite"
-        }
-      ]
-    }
-  ])
+  container_definitions = jsonencode([{
+    name      = "medusa",
+    image     = "medusajs/medusa:v1.13.1", # Use a valid Medusa version tag
+    portMappings = [{ containerPort = 9000 }],
+    environment = [
+      {
+        name  = "DATABASE_URL",
+        value = "sqlite://./medusa-db.sqlite"
+      }
+    ]
+  }])
 }
 
-# ECS Fargate Service
 resource "aws_ecs_service" "service" {
   name            = "medusa-service"
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.medusa_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
-
   network_configuration {
-    subnets          = [tolist(data.aws_subnet_ids.default.ids)[0]]
+    subnets          = [aws_subnet.public.id]
     security_groups  = [aws_security_group.ecs_sg.id]
     assign_public_ip = true
   }
-}
-
-# Output ECS Cluster name
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.cluster.name
 }
